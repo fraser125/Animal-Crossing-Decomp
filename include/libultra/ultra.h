8fa52aa6c90ab6475fab84df6f3e1908bd2d42e7
Cuyler36 <jeremy96@vt.edu>
2022-06-06 17:16:52 -0400

    libultra: Add osGetTime and other fixes

--- include/libultra/ultra.h
+++ include/libultra/ultra.h
@@ -6,6 +6,7 @@ extern "C" {
 #endif /* _LANGUAGE_C_PLUS_PLUS */
 
 #include <stddef.h> /* size_t */
+#include <dolphin/os.h>
 #include "types.h"
 
 void bcopy(void* __src, void* __dest, size_t __n);
@@ -14,6 +15,11 @@ void bzero(void* __s, size_t __n);
 void osSyncPrintf(const char* format, ...);
 void osWritebackDCache(void* vaddr, int nbytes);
 u32 osGetCount();
+OSTime osGetTime();
+
+static OSTime __osTimeOffset;
+
+extern BOOL osShutdown;
 
 #ifdef _LANGUAGE_C_PLUS_PLUS
 }
--- libs/libultra/ultra.c
+++ libs/libultra/ultra.c
@@ -4,35 +4,38 @@
 #include <stdarg.h>
 #include <__mem.h>
 #include <string.h>
-#include <dolphin/os.h>
 #include "ultra.h"
 #include "OSReport.h"
 
-void bcopy(void* __src, void* __dst, size_t __n) {
+extern void bcopy(void* __src, void* __dst, size_t __n) {
     memmove(__dst, __src, __n);
 }
 
-int bcmp(void* __s1, void* __s2, size_t __n) {
+extern int bcmp(void* __s1, void* __s2, size_t __n) {
     return memcmp(__s1, __s2, __n);
 }
 
-void bzero(void* __s, size_t __n) {
+extern void bzero(void* __s, size_t __n) {
     memset(__s, 0, __n);
 }
 
-void osSyncPrintf(const char* format, ...) {
+extern void osSyncPrintf(const char* format, ...) {
     va_list arg;
     va_start(arg, format);
     OSVReport(format, arg);
     va_end(arg);
 }
 
-void osWritebackDCache(void* vaddr, int nbytes) {
+extern void osWritebackDCache(void* vaddr, int nbytes) {
     DCStoreRange(vaddr, nbytes);
 }
 
-u32 osGetCount() {
+extern u32 osGetCount() {
     return OSGetTick();
+}
+
+extern OSTime osGetTime() {
+    return OSGetTime() + __osTimeOffset;
 }
 
 #endif
