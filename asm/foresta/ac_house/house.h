5afd96152586947daea3ce0cd43071b12f39df94
fraser <jkwall@hotmail.com>
2022-07-13 12:23:59 -0700

    Starter

---
+++ asm/foresta/ac_house/house.h
@@ -0,0 +1,14 @@
+void aHUS_actor_move(void*, s32);                      /* extern */
+void mFI_SetFG_common(s16, s32*, u32, s16);              /* extern */
+
+struct house {
+    s32 unk0;
+    s16 unk4;
+    s16 unk6;
+    s32 unk8;
+    s32 unkC;
+    s32 unk10;
+    s32 unk14;
+    void * unk164;
+};
+struct house house;
